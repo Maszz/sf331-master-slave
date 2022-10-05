ad6e01a46f3eead2f7ed9b873e829ef072f25654
Masz <mawin38408@gmail.com>
2022-10-05 17:26:50 +0700

    clean worktree

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { PrismaModule } from './prisma/prisma.module';
 @Module({
   imports: [PrismaModule],
